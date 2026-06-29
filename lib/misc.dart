978b2f87b96f4e74d819bd8d14d1ca197ffe1f57
nvkantonio <nvkantonio@gmail.com>
2026-06-29 10:46:41 +0200

    add feat: database callback completer processor

--- lib/misc.dart
+++ lib/misc.dart
@@ -1,3 +1,4 @@
 library;
 
 export 'src/utils/database_exceptions.dart';
+export 'src/utils/database_callback_completers_processes.dart';
---
+++ lib/src/utils/database_callback_completers_processes.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+import 'package:sqflite_common/sqflite.dart';
+import 'package:storage_sources_core/callback_completer.dart';
+
+class DatabaseCallbackCompletersProcesses extends CallbackCompletersProcesses {
+  DatabaseCallbackCompletersProcesses(this.openDatabaseImplementationCallback);
+
+  FutureOr<Database>? _database;
+
+  final FutureOr<Database> Function() openDatabaseImplementationCallback;
+
+  Future<R> runWithDb<R extends dynamic>(
+    FutureOr<R> Function(Database db) callback, {
+    required Object processLink,
+    equalityArg = const NoArgument(),
+  }) async {
+    final Database db;
+
+    switch (_database) {
+      case Database database when database.isOpen:
+        db = database;
+      case Database database when !database.isOpen:
+      case null:
+        _database = openDatabaseImplementationCallback();
+        db = await _database!;
+        _database = db;
+      default:
+        db = await _database!;
+    }
+
+    return run(
+      () => callback(db),
+      processLink: processLink,
+      equalityArg: equalityArg,
+    ).whenComplete(
+      () {
+        if (completersHashMap.isEmpty) {
+          switch (_database) {
+            case Database database when database.isOpen:
+              database.close();
+              _database = null;
+            case Database database when !database.isOpen:
+              _database = null;
+            default:
+              break;
+          }
+        }
+      },
+    );
+  }
+}
